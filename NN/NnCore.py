b5ec88187c4832dbc036ae915a64b361652b635b
Wojciech <wojciech.kur96@gmail.com>
2019-10-23 23:28:59 +0200

    First try (bad results)

--- NN/NnCore.py
+++ NN/NnCore.py
@@ -1,21 +1,34 @@
+import copy
 import numpy as np
+from NN.Perceptron import Perceptron
 
 
 class NnCore:
     def __init__(self, learning_constant):
         self.learning_constant = learning_constant
 
-    def learn(self, neuron, data, labels, result_number):
+    def learn(self, neuron_map, data, labels, result_number):
         error = 0
         error_rate = 0
+        mapped_neurons = []
         for i in range(len(data)):
-            neuron.set_data(data[i], labels[i])
-            result = neuron.sigmoid_function(result_number)
-            if round(result) != float(labels[i]):
-                error_rate += 1
-            error += np.power(result - labels[i], 2)
-            neuron.correction(result, self.learning_constant)
+            local_data = data[i]
+            for layer in neuron_map:
+                temp_data = []
+                for j in range(layer):
+                    neuron = Perceptron(len(local_data), 1.0)
+                    mapped_neurons.append(neuron)
+                    neuron.set_data(local_data, labels[i])
+                    result = neuron.sigmoid_function(result_number)
+                    temp_data.append(result)
+                    if layer == 1:
+                        if round(result) != float(labels[i]):
+                            error_rate += 1
+                        error += np.power(result - labels[i], 2)
+                    neuron.correction(result, self.learning_constant)
+                local_data = temp_data
+
         error /= len(data)
         error_rate /= len(data)
 
-        return error, (round(error_rate * 100, 2))
+        return error, (round(error_rate * 100, 2)), mapped_neurons
--- NnMain.py
+++ NnMain.py
@@ -1,7 +1,6 @@
 import copy
 import time
 from NN.NnCore import NnCore
-from NN.Perceptron import Perceptron
 from KNN.DataPreparation import DataPreparation
 
 
@@ -12,7 +11,8 @@ if __name__ == '__main__':
     nn = NnCore(0.15)
     generator = DataPreparation(125)
     learn_data, learn_labels = generator.create_learn_data()
-    neuron = Perceptron(len(learn_data[0]), 0.5)
+    test_data, test_labels = generator.create_test_data()
+    neuron_map = [4, 1]
 
     # ******************************* TESTS ******************************* #
 
@@ -23,13 +23,12 @@ if __name__ == '__main__':
     error_sum_rates = 1
     neuron_best_rate = None
     while smallest_error > 0.1 and time.time() - start_time < 30:
-        error, error_rate = nn.learn(neuron, learn_data, learn_labels, 3)
+        error, error_rate, mapped_neurons = nn.learn(neuron_map, learn_data, learn_labels, 3)
         if error < smallest_error:
             smallest_error = error
-            neuron_state = copy.deepcopy(neuron)
-            print('Error: ', error, 'Error rate: ', error_rate.__str__() + "%")
+            neuron_state = copy.deepcopy(mapped_neurons[-1])
+            print('Error: ', error, 'Error rate: ', error_rate.__str__() + '%')
             temp_error_rate = 0
-            test_data, test_labels = generator.create_test_data()
             for i in range(len(test_data)):
                 neuron_state.set_data(test_data[i])
                 result = neuron_state.sigmoid_function(3)
@@ -40,13 +39,13 @@ if __name__ == '__main__':
             if sum(temp_rate) <= sum(smallest_sum_rates):
                 smallest_sum_rates = temp_rate
                 error_sum_rates = error
-                neuron_best_rate = copy.deepcopy(neuron)
+                neuron_best_rate = copy.deepcopy(mapped_neurons[-1])
 
-            print("Test error rate: ", temp_rate[0], "%")
+            print('Test error rate: ', temp_rate[0], '%')
 
-    print("\nBest neuron state with error: ", smallest_error)
-    print("Weights: ", neuron_state.weights)
+    print('\nBest neuron state with error: ', smallest_error)
+    print('Weights: ', neuron_state.weights)
 
-    print("\nBest neuron state for test with error: ", error_sum_rates)
-    print("Test rate: ", smallest_sum_rates[0].__str__() + "%", "Learn rate: ", smallest_sum_rates[1].__str__() + "%")
-    print("Weights: ", neuron_best_rate.weights)
+    print('\nBest neuron state for test with error: ', error_sum_rates)
+    print('Test rate: ', smallest_sum_rates[0].__str__() + '%', 'Learn rate: ', smallest_sum_rates[1].__str__() + '%')
+    print('Weights: ', neuron_best_rate.weights)
