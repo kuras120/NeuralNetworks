72d6e5530211f531e94c706da0479658f02384a3
Wojciech Kur <wojciech.kur96@gmail.com>
2021-04-11 10:22:43 +0200

    Handler for bash script on WSL

--- chess_runtime.sh
+++ chess_runtime.sh
@@ -1,1 +1,4 @@
 python Chess/Process.py 4 null null null null null null null null null null null null null null null null 0 0
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+  echo -e "\n" && read -n 1 -s -r -p "Press any key to continue..." && echo -e "\n"
+fi
