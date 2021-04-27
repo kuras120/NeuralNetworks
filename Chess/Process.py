79bab055072f31ea5fa0048084144881b5a57cc9
Wojciech Kur <wojciech.kur96@gmail.com>
2021-04-27 23:14:40 +0200

    State handler PoC

--- Chess/Process.py
+++ Chess/Process.py
@@ -1,22 +1,49 @@
+import os
 import sys
+import json
 
 
 class Process:
-    def __init__(self):
-        self.__length = int(sys.argv[1])
-        temp = sys.argv[2:-2]
-        self.__array = [temp[i:i + self.__length] for i in range(0, len(temp), self.__length)]
+    def __init__(self, reset_on_new, learning_mode, state_file_path):
+        self.__matrix = []
+        self.__q_table = {}
         self.__points = sys.argv[-2:]
+        self.__length = int(sys.argv[1])
+        self.__reset_on_new = reset_on_new
+        self.__learning_mode = learning_mode
+        self.__hash = ''.join(sys.argv[2:-2])
+        self.init(sys.argv[2:-2], state_file_path.replace('\\', '/'))
+
+    def init(self, matrix, state_file_path):
+        init_state = True
+        for i in range(0, len(matrix), self.__length):
+            sub_list = matrix[i:i + self.__length]
+            self.__matrix.append(sub_list)
+            if sub_list.count(sub_list[0]) != len(sub_list):
+                init_state = False
+        absolute_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), state_file_path)
+        if init_state and self.__reset_on_new:
+            for i in range(len(self.__matrix)):
+                for j in range(len(self.__matrix[i])):
+                    self.__q_table[str(i) + ':' + str(j) + ':' + self.__hash] = 0
+            with open(absolute_path, 'w') as state:
+                json.dump(self.__q_table, state)
+            print('State saved in ' + absolute_path)
+        else:
+            with open(absolute_path, 'r') as state:
+                self.__q_table = json.load(state)
 
     def values(self):
         print('Macierz stanu:')
-        for elem in self.__array:
+        for elem in self.__matrix:
             print('     ', elem)
         print('Punkty:')
         print('     Gracz: ' + self.__points[0])
         print('     AI: ' + self.__points[1])
+        print('Stan:')
+        print(json.dumps(self.__q_table, indent=2))
 
 
 if __name__ == '__main__':
-    process = Process()
+    process = Process(True, True, 'Data/state.json')
     process.values()
--- chess_runtime.sh
+++ chess_runtime.sh
@@ -1,4 +1,4 @@
-python Chess/Process.py 4 null null null null null null null null null null null null null null null null 0 0
+python Chess/Process.py 4 N N N N N N N N N N N N N N N N 0 0
 if [[ "$OSTYPE" != "linux-gnu"* ]]; then
   echo -e "\n" && read -n 1 -s -r -p "Press any key to continue..." && echo -e "\n"
 fi
