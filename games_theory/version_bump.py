347aadf05e168e03e8a54182f47c2a179d427bfd
Wojciech <wojciech.kur96@gmail.com>
2025-09-28 00:27:02 +0200

    Old version of bump

--- games_theory/version_bump.py
+++ games_theory/version_bump.py
@@ -1,14 +1,17 @@
 import sys
-from games_theory import __version__
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Bump version (bump) or return actual version (version)?')
         exit(1)
-    rel_version = __version__.split('.')
+    with open('games_theory/__init__.py', 'r') as file:
+        version = file.readline().split('=')[1].strip().strip('"')
+        rel_version = version.split('.')
     if sys.argv[1] == 'bump':
         rel_version[2] = str(int(rel_version[2]) + 1)
-        __version__ = '.'.join(rel_version)
+        bumped_version = '.'.join(rel_version)
+        with open('games_theory/__init__.py', 'w') as file:
+            file.write('__version__ = "' + bumped_version + '"')
     if sys.argv[1] == 'version':
-        sys.stdout.write(__version__)
+        sys.stdout.write(version)
         sys.stdout.flush()
